2a3cfa4cb330949cd0502b37e72dc09e9ccde5c3
Julia Karnausova <julianaas@yandex.ru>
2024-01-25 18:49:01 +0300

    feat: show cards from mocks

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,5 +1,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination, Autoplay } from 'swiper/modules';
+import { PromoCards } from '../../mocks/promo';
+
 import 'swiper/css';
 import 'swiper/css/pagination';
 
@@ -10,7 +12,7 @@ function Banner(): JSX.Element {
   const pagination = {
     clickable: true,
     renderBullet: function (index, className) {
-      return `<span class="${ className }"></span>`;
+      return `<span class="${className}"></span>`;
     },
   };
 
@@ -18,96 +20,39 @@ function Banner(): JSX.Element {
     <Swiper
       pagination={pagination}
       modules={[Pagination]}
-      className='swiper'
+      className='bannerSwiper'
     >
 
-      <SwiperSlide>
-        <div className="banner">
-          <picture>
-            <source
-              type="image/webp"
-              srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"
-            />
-            <img
-              src="img/content/banner-bg.jpg"
-              srcSet="img/content/banner-bg@2x.jpg 2x"
-              width={1280}
-              height={280}
-              alt="баннер"
-            />
-          </picture>
-          <p className="banner__info">
-            <span className="banner__message">Новинка!</span>
-            <span className="title title--h1">
-        Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i
-            </span>
-            <span className="banner__text">
+      {PromoCards.map(({id, name, previewImg, previewImg2x, previewImgWebp, previewImgWebp2x}) => (
+        <SwiperSlide key={id}>
+          <div className="banner">
+            <picture>
+              <source
+                type="image/webp"
+                srcSet={`${previewImgWebp}, ${previewImgWebp2x} 2x"`}
+              />
+              <img
+                src={previewImg}
+                srcSet={`${previewImg2x} 2x`}
+                width={1280}
+                height={280}
+                alt="баннер"
+              />
+            </picture>
+            <p className="banner__info">
+              <span className="banner__message">Новинка!</span>
+              <span className="title title--h1">
+                {name}
+              </span>
+              <span className="banner__text">
         Профессиональная камера от&nbsp;известного производителя
-            </span>
-            <a className="btn" href="#">
+              </span>
+              <a className="btn" href="#">
         Подробнее
-            </a>
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="banner">
-          <picture>
-            <source
-              type="image/webp"
-              srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"
-            />
-            <img
-              src="img/content/banner-bg.jpg"
-              srcSet="img/content/banner-bg@2x.jpg 2x"
-              width={1280}
-              height={280}
-              alt="баннер"
-            />
-          </picture>
-          <p className="banner__info">
-            <span className="banner__message">Новинка!</span>
-            <span className="title title--h1">
-        Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i
-            </span>
-            <span className="banner__text">
-        Профессиональная камера от&nbsp;известного производителя
-            </span>
-            <a className="btn" href="#">
-        Подробнее
-            </a>
-          </p>
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="banner">
-          <picture>
-            <source
-              type="image/webp"
-              srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"
-            />
-            <img
-              src="img/content/banner-bg.jpg"
-              srcSet="img/content/banner-bg@2x.jpg 2x"
-              width={1280}
-              height={280}
-              alt="баннер"
-            />
-          </picture>
-          <p className="banner__info">
-            <span className="banner__message">Новинка!</span>
-            <span className="title title--h1">
-        Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i
-            </span>
-            <span className="banner__text">
-        Профессиональная камера от&nbsp;известного производителя
-            </span>
-            <a className="btn" href="#">
-        Подробнее
-            </a>
-          </p>
-        </div>
-      </SwiperSlide>
+              </a>
+            </p>
+          </div>
+        </SwiperSlide>))}
     </Swiper>
   );
 }
