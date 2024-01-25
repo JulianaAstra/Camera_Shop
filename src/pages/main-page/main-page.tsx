0049842a38a114dd13b64b40f6cd43080b52af23
Julia Karnausova <julianaas@yandex.ru>
2024-01-25 17:22:48 +0300

    add banner component + install swiper

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -8,6 +8,7 @@ import PaginationComponent from '../../components/pagination/pagination';
 import { useEffect } from 'react';
 import { usePagination } from '../../components/pagination/pagination-context';
 import { useParams } from 'react-router-dom';
+import Banner from '../../components/banner/banner';
 
 function MainPageComponent(): JSX.Element {
 
@@ -133,33 +134,8 @@ function MainPageComponent(): JSX.Element {
         </div>
       </header>
       <main>
-        <div className="banner">
-          <picture>
-            <source
-              type="image/webp"
-              srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"
-            />
-            <img
-              src="img/content/banner-bg.jpg"
-              srcSet="img/content/banner-bg@2x.jpg 2x"
-              width={1280}
-              height={280}
-              alt="баннер"
-            />
-          </picture>
-          <p className="banner__info">
-            <span className="banner__message">Новинка!</span>
-            <span className="title title--h1">
-          Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i
-            </span>
-            <span className="banner__text">
-          Профессиональная камера от&nbsp;известного производителя
-            </span>
-            <a className="btn" href="#">
-          Подробнее
-            </a>
-          </p>
-        </div>
+        <Banner />
+
         <div className="page-content">
           <div className="breadcrumbs">
             <div className="container">
